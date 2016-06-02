dcf675123eece633c208f160119a42d804c0bbad
romeu <romeu@enfants.co>
2016-06-02 14:28:39 -0300

    Start contact CRUD

---
+++ app/views/contacts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @contact, :id, :name, :email, :age, :stage, :office, :created_at, :updated_at
---
+++ db/migrate/20160602165455_create_contacts.rb
@@ -0,0 +1,13 @@
+class CreateContacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :email
+      t.integer :age
+      t.string :stage
+      t.string :office
+
+      t.timestamps null: false
+    end
+  end
+end
